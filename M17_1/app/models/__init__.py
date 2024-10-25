f1ff3fcf237b2fa7226d54c234d166a843db1640
OlegMSU56 <sagdeev.ol@yandex.ru>
2024-10-25 18:10:51 +0300

    25/10/24 задание 17_2

--- M17_1/app/models/__init__.py
+++ M17_1/app/models/__init__.py
@@ -1,2 +1,8 @@
-from .user import User
-from .task import Task
+from M17_1.app.models.user import User
+from M17_1.app.models.task import Task
+from sqlalchemy.schema import CreateTable
+
+
+
+print(CreateTable(Task.__table__))
+print(CreateTable(User.__table__))
